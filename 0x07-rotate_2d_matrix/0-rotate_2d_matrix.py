caf9957e1a55fe56ff36c5b2036b16965d648990
leulkashay <leulkashay2012@gmail.com>
2023-08-16 06:59:46 +0000

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,7 +5,7 @@
 def rotate_2d_matrix(matrix):
     """ a fun rotate 2d matrix 90 degree"""
     matrix_len = len(matrix)
-    copy_matrix = [[ matrix[x][y] for y in range(matrix_len)] for x in range(matrix_len)]
+    copy_matrix = [[matrix[x][y] for y in range(matrix_len)] for x in range(matrix_len)]
     for x in range(matrix_len):
         for y in range(matrix_len):
             matrix[y][matrix_len-1-x] = copy_matrix[x][y]
